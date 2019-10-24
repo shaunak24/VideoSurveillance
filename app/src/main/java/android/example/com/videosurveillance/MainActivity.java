9b922444f2f2d0d0b61a62095a1f14fe0a1142e3
Shaunak <shaunak241997@gmail.com>
2019-10-24 13:19:31 +0530

    First Commit Swipe Gestures added

--- app/src/main/java/android/example/com/videosurveillance/MainActivity.java
+++ app/src/main/java/android/example/com/videosurveillance/MainActivity.java
@@ -1,6 +1,5 @@
 package android.example.com.videosurveillance;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,21 +8,45 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.widget.Toast;
-import android.widget.VideoView;
 
+import com.longdo.mjpegviewer.MjpegView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.util.HashMap;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private VideoView videoView;
+    private MjpegView videoView;
     private GestureDetector gestureDetector;
     private View.OnTouchListener gestureListener;
+    private String direction;
+    private String motion;
+    private Boolean flag_up;
+    private Boolean flag_left;
+    private Boolean flag_down;
+    private Boolean flag_right;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        direction = "s";
+        motion = "s";
+        flag_up = true;
+        flag_left = true;
+        flag_down = true;
+        flag_right = true;
         videoView = findViewById(R.id.video_view);
         gestureDetector = new GestureDetector(this, new SwipeGestureDetector());
         gestureListener = new View.OnTouchListener() {
@@ -34,11 +57,14 @@ public class MainActivity extends AppCompatActivity {
         };
         videoView.setOnTouchListener(gestureListener);
 
-        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
-        videoView.setVideoURI(uri);
-        //videoView.setVideoPath("http://192.168.43.250:8000/video/");
-
-        videoView.start();
+//        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
+//        videoView.setVideoURI(uri);
+//        videoView.setVideoPath("http://192.168.43.100:8000/");
+//        videoView.start();
+        videoView.setMode(MjpegView.MODE_FIT_WIDTH);
+        videoView.setAdjustHeight(true);
+        videoView.setUrl("http://192.168.43.100:8000/");
+        videoView.startStream();
     }
 
     class SwipeGestureDetector extends GestureDetector.SimpleOnGestureListener {
@@ -48,33 +74,58 @@ public class MainActivity extends AppCompatActivity {
             try {
                 HashMap<String, Integer> valuesForSwipe = getValuesForSwipe();
 
-                if(Math.abs(e1.getX() - e2.getX()) > Math.abs(e1.getY() - e2.getY())) {
+                if (Math.abs(e1.getX() - e2.getX()) > Math.abs(e1.getY() - e2.getY())) {
                     // Left swipe
                     if (e1.getX() - e2.getX() > valuesForSwipe.get("swipeMinDistance")
                             && Math.abs(velocityX) > valuesForSwipe.get("swipeThresholdVelocity")) {
                         showToast("LEFT");
+                        if (flag_left) {
+                            direction = "l";
+                        } else {
+                            direction = "s";
+                        }
+                        flag_left = !flag_left;
                     }
                     // Right swipe
                     if (e2.getX() - e1.getX() > valuesForSwipe.get("swipeMinDistance")
                             && Math.abs(velocityX) > valuesForSwipe.get("swipeThresholdVelocity")) {
                         showToast("RIGHT");
+                        if (flag_right) {
+                            direction = "r";
+                        } else {
+                            direction = "s";
+                        }
+                        flag_right = !flag_right;
                     }
-                }
-                else {
+                } else {
                     // Swipe down
                     if (e2.getY() - e1.getY() > valuesForSwipe.get("swipeMinDistance")
                             && Math.abs(velocityY) > valuesForSwipe.get("swipeThresholdVelocity")) {
                         showToast("DOWN");
+                        if (flag_down) {
+                            motion = "b";
+                        } else {
+                            motion = "s";
+                        }
+                        flag_down = !flag_down;
                     }
                     // Swipe up
                     if (e1.getY() - e2.getY() > valuesForSwipe.get("swipeMinDistance")
                             && Math.abs(velocityX) > valuesForSwipe.get("swipeThresholdVelocity")) {
                         showToast("UP");
+                        if (flag_up) {
+                            motion = "f";
+                        } else {
+                            motion = "s";
+                        }
+                        flag_up = !flag_up;
                     }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            // send direction, motion coordinates
+            //sendCoordinates();
             return false;
         }
 
@@ -83,6 +134,27 @@ public class MainActivity extends AppCompatActivity {
             Log.d("tag", "onDown: " + event.toString());
             return true;
         }
+    }
+
+    private void sendCoordinates() {
+        String url = "http://192.168.93.250:8000/";
+        RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("direction", direction)
+                .addFormDataPart("motion", motion)
+                .build();
+
+        Call call = sendHttpPostRequest(url, requestBody);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+            }
+        });
     }
 
     HashMap<String, Integer> getValuesForSwipe() {
@@ -95,5 +167,23 @@ public class MainActivity extends AppCompatActivity {
         return swipeValues;
     }
 
-    private void showToast(String msg) {Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();}
+    private void showToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public static Call sendHttpPostRequest(String url, RequestBody requestBody)
+    {
+        try
+        {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request.Builder().url(url).post(requestBody).build();
+            return okHttpClient.newCall(request);
+        }
+        catch (Exception e)
+        {
+            Log.e("OkHttpUtils", e.toString());
+            return null;
+        }
+    }
+
 }
